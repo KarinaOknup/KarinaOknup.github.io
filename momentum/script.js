e6fe69dd840f45c8e43488d6c5e5e6eca45b6885
Karina <karina.oknup@gmail.com>
2020-10-25 00:33:41 +0300

    add: dayOfWeek , day month

--- momentum/script.js
+++ momentum/script.js
@@ -3,9 +3,7 @@ const time = document.querySelector('.time'),
   greeting = document.querySelector('.greeting'),
   name = document.querySelector('.name'),
   focus = document.querySelector('.focus');
-
-// Options
-const showAmPm = true;
+  data = document.querySelector('.data');
 
 // Show Time
 function showTime() {
@@ -13,15 +11,21 @@ function showTime() {
     hour = today.getHours(),
     min = today.getMinutes(),
     sec = today.getSeconds();
+    day = today.getDate(); 
+    dayW = today.getDay();
+    month = today.getMonth();
 
-  // Set AM or PM
-  const amPm = hour >= 12 ? 'PM' : 'AM';
-
-  // Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    // Output Time
+  time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(
     sec
   )}`;
 
+let weekday =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let dayWeek=weekday[dayW];
+
+let monthName = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+let monthNow=monthName[month];
+  data.innerHTML = `${dayWeek}<span> , </span>${day} ${monthNow} `
   setTimeout(showTime, 1000);
 }
 
@@ -35,21 +39,27 @@ function setBgGreet() {
   let today = new Date(),
     hour = today.getHours();
 
-  if (hour < 12) {
+  if (hour <= 12 && hour > 6) {
     // Morning
     document.body.style.backgroundImage =
       "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
     greeting.textContent = 'Good Morning, ';
-  } else if (hour < 18) {
-    // Afternoon
+  } else if (hour <= 18 && hour > 12) {
+    // Day
     document.body.style.backgroundImage =
-      "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-    greeting.textContent = 'Good Afternoon, ';
-  } else {
+      "url('https://i.ibb.co/3mThcXc/evening.jpg')";
+    greeting.textContent = 'Good Day, ';
+  } else if (hour <= 24 && hour > 18){
     // Evening
     document.body.style.backgroundImage =
       "url('https://i.ibb.co/924T2Wv/night.jpg')";
     greeting.textContent = 'Good Evening, ';
+    document.body.style.color = 'white';
+     } else if (hour <= 6){
+    // Night
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/924T2Wv/night.jpg')";
+    greeting.textContent = 'Good Night, ';
     document.body.style.color = 'white';
   }
 }
