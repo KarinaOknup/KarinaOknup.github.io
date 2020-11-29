c710697828697b3be4aeba21e067aafed82363ea
Karina <karina.oknup@gmail.com>
2020-11-30 02:47:51 +0300

    feat: draft for game

--- english-for-kids/src/index.js
+++ english-for-kids/src/index.js
@@ -2,7 +2,7 @@ import {MODES} from "./js/MODES";
 import {input}  from "./js/data";
 import 'bootstrap';
 
-const mainPage = document.querySelector('#main');//Main Page
+const main = document.querySelector('#main');//Main Page
 const pageContainer=document.querySelector('#pageContainer');
 // const categoryPage = document.querySelector('#category');// Category Page
 
@@ -76,26 +76,48 @@ class Card {
       //create card//
       createTrainCard(){
         const card = document.createElement('div');
-        card.className='card';
-        const front=document.createElement('div');
-        front.className='front';
-        front.innerHTML=`
-          <img src="${input[this.theme][this.numberOfword].image}" class="card-img-top" alt="...">
-          <div class="card-body">
-          <p class="card-text">${input[this.theme][this.numberOfword].word}</p>
-          </div>
-          `
+        card.className = 'card';
+        //FRONT//
+        const front = document.createElement('div');
+        front.className = 'front';
+        //---------------///
+        const imgF = document.createElement('img');
+        imgF.src = `${input[this.theme][this.numberOfword].image}`;
+        imgF.className ='card-img-top';
+        imgF.alt = `${input[this.theme][this.numberOfword].word}`;
+        front.appendChild(imgF);
+        //---------------///
+        const cardBody = document.createElement('div')
+        cardBody.className = 'card-body';
+        //---------------///
+        const text = document.createElement('p');
+        text.className = 'card-text';
+        text.innerHTML = `${input[this.theme][this.numberOfword].word}`;
+        cardBody.appendChild(text);
+        front.appendChild(cardBody);
         card.appendChild(front);
-        const back=document.createElement('div');
-        back.className='back';
-        back.innerHTML=`
-          <img src="${input[this.theme][this.numberOfword].image}" class="card-img-top" alt="...">
-          <div class="card-body">
-          <p class="card-text">${input[this.theme][this.numberOfword].translation}</p>
-          </div>
-          `
+        //BACK//
+        const back = document.createElement('div');
+        back.className = 'back';
+        //---------------///
+        const imgB = document.createElement('img');
+        imgB.src = `${input[this.theme][this.numberOfword].image}`;
+        imgB.className ='card-img-top';
+        imgB.alt = `${input[this.theme][this.numberOfword].translation}`;
+        back.appendChild(imgB);
+        //---------------///
+        const cardBodyB = document.createElement('div')
+        cardBodyB.className = 'card-body';
+        //---------------///
+        const textB = document.createElement('p');
+        textB.className = 'card-text';
+        textB.innerHTML = `${input[this.theme][this.numberOfword].translation}`;
+        cardBodyB.appendChild(textB);
+        back.appendChild(cardBodyB);
         card.appendChild(back);
+        //---------------///
         pageContainer.appendChild(card);
+
         //FLIP//
         card.addEventListener('click', function() {
         card.classList.toggle('flipped');
@@ -108,46 +130,73 @@ class Card {
         const card = document.createElement('div');
         card.className='card';
         card.style='background:rgb(148, 66, 195)';
-
+        //---------------///
         const img=document.createElement('img');
         img.src=`${input[this.theme][this.numberOfword].image}`;
         img.className='card-img-top';
         img.alt=`${input[this.theme][this.numberOfword].word}`;
         card.appendChild(img);
-
+        //---------------///
         const cardBody=document.createElement('div')
         cardBody.className='card-body';
         const text=document.createElement('p');
-
+        //---------------///
         text.className='card-text play-text';
         text.innerHTML=`${input[this.theme][this.numberOfword].translation}`;
         cardBody.appendChild(text);
         card.appendChild(cardBody);
-
+        //---------------///
         pageContainer.appendChild(card);
         //OnClick-draft for game//
         card.addEventListener('click', function() {
           card.style='opacity:0.5;';
           text.style='visibility:visible;';
+          star.isCorrect();
           });
       }
       setMode(mode){
         this.mode = mode;
       }
-}
-//CREATE ELEMENT//
-for(let i = 0 ; i < 8 ; i++){
-let card = new Card(3,i);
-switch(card.mode){
-  case 'train' :
-  card.createTrainCard();
+};
+//SCALE//
+let star = {
+  stars:0,
+  createScale(){
+  const scale = document.createElement('div');
+  scale.id='scale';
+  main.prepend(scale);
+  },
+  isCorrect(){
+  const star=document.createElement('div');
+  star.className='star';
+  document.querySelector('#scale').prepend(star);
+  this.stars++
+  },
+  isUncorrect(){
+    const star=document.createElement('div');
+    star.className='empty-star';
+    document.querySelector('#scale').prepend(star);
+    }
+  }
 
+//CREATE ELEMENT//
+// switch(card.mode){
+switch('play'){
+  case 'train' :
+  for(let i = 0 ; i < 8 ; i++){
+  let card = new Card(3,i);
+  card.createTrainCard();
+  }
   break;
   case 'play' :
-  card.createPlayCard();
+  star.createScale();
+  for(let i = 0 ; i < 8 ; i++){
+      let card = new Card(3,i);
+      card.createPlayCard();
+  }
   break;
 }
-}
+
 
 
 
