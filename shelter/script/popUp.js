7631e060182d6020ee8646449eec8fa66a99909b
KarinaOknup <punko.karina@yandex.by>
2021-01-12 22:35:25 +0300

    add: popBox start

--- shelter/script/popUp.js
+++ shelter/script/popUp.js
@@ -1,11 +1,24 @@
-console.log('arrPets');
-document.getElementsByClassName('slide-items').forEach(e=>{
-  e.addEventListener('click',popUp())
-})
-function popUp(){
-  console.log('arrPets');
-  let popBox=document.createElement('div');
-  popBox.className('popBox');
-  popBox.addEventListener('click',function(){alert('smile')});
+const arrPets = document.querySelectorAll('.slide-items');
+Array.from(arrPets).forEach( (e)=>{
+  e.addEventListener('click', function(){
+  let popBox = document.createElement('div');
+  let shadow = document.createElement('div');
+  shadow.className = 'shadow';
+  popBox.className = 'popBox';
+  let popMain = document.createElement('div');
+  let cross = document.createElement('div');
+  cross.className = 'cross';
+  cross.innerHTML = '<img alt = "close" src = "assets/icons/cancel.svg">';
+  popBox.appendChild(cross);
+  popMain.className = 'popMain';
+  popBox.appendChild(popMain);
+  document.querySelector('body').appendChild(shadow);
   document.querySelector('body').appendChild(popBox);
-}
+  cross.addEventListener('click',function(){
+    popBox.remove();
+    shadow.remove();
+  });
+});
+})
+
+
