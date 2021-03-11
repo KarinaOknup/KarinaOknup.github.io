1c5c9be9be2ef35ffc699773e5d6034dea47bcf6
KarinaOknup <punko.karina@yandex.by>
2021-03-12 01:05:18 +0300

    add:start css

--- wild-life/wild-life/index.js
+++ wild-life/wild-life/index.js
@@ -1,16 +1,5 @@
-// import Swiper from 'swiper';
 import {slides} from './assets/data-slides';
 
-// const swiper = new Swiper('.swiper-container', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
 slides.forEach(e => {
   let slide = document.createElement('div');
   slide.className = 'swiper-slide';
