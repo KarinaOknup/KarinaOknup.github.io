5e40ac348971cd7abd2c66a28f99e19dd3d24b13
KarinaOknup <punko.karina@yandex.by>
2021-03-14 01:42:35 +0300

    add: start doing slide-show

--- wild-life/wild-life/index.js
+++ wild-life/wild-life/index.js
@@ -1,10 +1,29 @@
-import {slides} from './assets/data-slides';
-
+let slides = [
+  {
+    id:0,
+    name:"Deer",
+    description:"Naturalist investigation",
+    img:'./assets/slides/deer.jpg',
+  },
+  {
+    id:1,
+    name:"Squirrel",
+    description:"Kamikaze squirrels",
+    img:'./assets/slides/squirrel.jpg',
+  },
+  {
+    id:2,
+    name:"Bird",
+    description:"Birds Fight club",
+    img:'./assets/slides/bird.jpg',
+  }
+];
+let slideBox = document.getElementsByClassName('swiper-wrapper');
 slides.forEach(e => {
   let slide = document.createElement('div');
   slide.className = 'swiper-slide';
-  slide.style = `background-img:url(${e.img})`;
+  slide.style = `background-image:url('${e.img}')`;
   slide.innerHTML = `<h4>${e.name}</h4><span>${e.description}</span>`;
-  document.getElementsByClassName('swiper-wrapper').append(slide);
+  slideBox[0].append(slide);
 })
 
