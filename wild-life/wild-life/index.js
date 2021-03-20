71260a3893cf274e7ca31856a991075a3d99c906
KarinaOknup <punko.karina@yandex.by>
2021-03-20 14:19:00 +0300

    add: little marks

--- wild-life/wild-life/index.js
+++ wild-life/wild-life/index.js
@@ -37,4 +37,15 @@ var swiper = new Swiper('.swiper-container', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+      },
 });
