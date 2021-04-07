d968a19cbcc782f01e0572563da38de7062c53cd
KarinaOknup <punko.karina@yandex.by>
2021-04-07 11:23:34 +0300

    add: reset button

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -5,4 +5,36 @@ function handleUpdate() {
   document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix)
 }
 
-inputs.forEach(input => input.addEventListener('input', handleUpdate))
+inputs.forEach(input => input.addEventListener('input', handleUpdate));
+
+let reset = document.querySelector('.reset');
+let eventInput = new Event("input");
+reset.addEventListener('click',()=>{
+inputs.forEach(input => {
+    switch(input.name){
+    case 'blur':
+        input.value = 0;
+        break;
+    case 'saturate':
+        input.value = 1;
+        break;
+    case 'invert':
+        input.value = 0;
+        break;
+    case 'hue-rotate':
+        input.value = 0;
+        break;
+    case 'sepia':
+        input.value = 0;
+        break;
+    case 'brightness':
+        input.value = 1;
+        break;
+    default:
+        console.log('ERROR IN SWITCH-RESET')
+        break;
+  };
+  input.dispatchEvent(eventInput);
+})
+
+})
